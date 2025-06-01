c3dd66e5920fd3048e5a88e775e3f45007e9ecb1
João Vitor Soares <joaovitorsoaresdemoraes59@gmail.com>
2025-06-01 17:12:09 -0300

    Add carrossel de fotos e organizando nomes

---
+++ assets/js/carousel.js
@@ -0,0 +1,38 @@
+const images = document.querySelector(".carousel-images");
+const total = images.children.length;
+let index = 0;
+let intervalId;
+
+function showSlide(i) {
+  index = (i + total) % total;
+  images.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+  showSlide(index + 1);
+}
+
+function prevSlide() {
+  showSlide(index - 1);
+}
+
+document.querySelector(".next").onclick = () => {
+  nextSlide();
+  resetAutoPlay();
+};
+
+document.querySelector(".prev").onclick = () => {
+  prevSlide();
+  resetAutoPlay();
+};
+
+function startAutoPlay() {
+  intervalId = setInterval(nextSlide, 5000);
+}
+
+function resetAutoPlay() {
+  clearInterval(intervalId);
+  startAutoPlay();
+}
+
+startAutoPlay();
